c3178de6fffca00924462f1b7eef2fce5b7dd8a2
alpha <ansh.anand100@gmail.com>
2024-10-16 15:43:55 +0530

    removed other beans to not create a circular depencency

--- src/main/java/com/example/jwtAuthentication/config/SecurityConfig.java
+++ src/main/java/com/example/jwtAuthentication/config/SecurityConfig.java
@@ -1,31 +1,17 @@
 package com.example.jwtAuthentication.config;
 
 import com.example.jwtAuthentication.jwt.JwtAuthenticationFilter;
-import com.example.jwtAuthentication.model.User;
-import com.example.jwtAuthentication.repository.UserRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.ProviderManager;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
-import java.util.Optional;
 
 @Configuration
 @EnableWebSecurity
@@ -65,10 +51,5 @@ public class SecurityConfig {
 
         return http.build();
     }
-
-
-
-
-
 }
 
