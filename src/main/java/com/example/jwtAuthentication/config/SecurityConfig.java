2f578f3eadb9cccd3fa3c814dd230028801b006c
alpha <ansh.anand100@gmail.com>
2024-10-16 15:26:12 +0530

    removed other beans to not create a circular depencency

--- src/main/java/com/example/jwtAuthentication/config/SecurityConfig.java
+++ src/main/java/com/example/jwtAuthentication/config/SecurityConfig.java
@@ -31,12 +31,12 @@ import java.util.Optional;
 @EnableWebSecurity
 @Slf4j
 public class SecurityConfig {
-    private final UserRepository userRepository;
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final AuthenticationManager authenticationManager;
 
-    public SecurityConfig(UserRepository userRepository, JwtAuthenticationFilter jwtAuthenticationFilter) {
-        this.userRepository = userRepository;
+    public SecurityConfig(JwtAuthenticationFilter jwtAuthenticationFilter, AuthenticationManager authenticationManager) {
         this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+        this.authenticationManager = authenticationManager;
     }
 
 
@@ -60,49 +60,15 @@ public class SecurityConfig {
                 .sessionManagement(session->
                         session
                                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)) //jwt is stateless so spring wont create sessions
-                .authenticationManager(authenticationManager()) //sets the custom authentication Manager we made in Spring Security
+                .authenticationManager(authenticationManager) //sets the custom authentication Manager we made as a bean in Spring Security
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class); // adds custom jwt filter in the filter chain before the basic user/password filter
 
         return http.build();
     }
 
 
-    /**
-     * Password Encoder bean accessible everywhere via auto wiring
-     */
-    @Bean
-    public PasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder();
-    }
 
 
-    /**
-     * UserDetailService Functional Interface implemented as lambda exposed as a bean accessible everywhere via auto wiring. <br>
-     * Used by DaoAuthenticationProvider to fetch UserDetails object from Database
-     */
-    @Bean
-    public UserDetailsService userDetailsService(){
-        return username -> {
-            Optional<User> user = userRepository.findUserByUsernameEquals(username);
-            if(user.isPresent()){
-                log.info("user found : {}", user);
-                return user.get();
-            }
-            log.info("user not found for username: {}", username);
-            throw new UsernameNotFoundException("User with username \"" + username + "\" not found");
-        };
-    }
 
-
-    /**
-     * Custom Authentication Manager bean for custom login in controller, only needed if making custom UserPasswordAuthenticationToken and manually setting the authentication
-     */
-    @Bean
-    public AuthenticationManager authenticationManager() throws Exception { // AuthManager is the one that handles authentication. ProviderManager is the default implementation of AuthenticationManager. HttpBasic does this automatically.
-        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider(); // type of authentication provider that does certain kind of authentication (gets username/password from UserDetailService). Spring makes this authentication manager with dao provider by default. Use AuthenticationConfiguration config, config.getAuthenticationManager() to get default AuthenticationManager used by spring security which auto wires our beans like password encrypter and userDetailService. Use this when customizing the auth manager.
-        authenticationProvider.setPasswordEncoder(passwordEncoder());
-        authenticationProvider.setUserDetailsService(userDetailsService()); // we can use AuthenticationConfiguration config
-        return new ProviderManager(authenticationProvider); //The AuthenticationManager (typically ProviderManager) goes through the authentication filters (e.g., DaoAuthenticationProvider) to check if the credentials are valid.
-    }
 }
 
